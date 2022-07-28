489c209b18dad3f2a0da7d8828a74250832ecde2
Mitch_L <101993290+Mitch-L@users.noreply.github.com>
2022-07-28 10:10:36 -0500

    Added Meals.

--- RecommendationProject/data.py
+++ RecommendationProject/data.py
@@ -1,10 +1,9 @@
 from os import link
 
 
-types = ["Chicken", "Beef", "Pork", "Sausage"]
+types = ["Chicken", "Beef", "Pork"]
 
 
-meals = [["Chicken", "Creamy Chicken and Rice skillet"],
-["Chicken", "Tuscan Grilled Chicken with Strawberry Salad",],
+meals = [["Chicken", "Tuscan Grilled Chicken with Strawberry Salad"],
 ["Chicken", "Grilled Garlic and Herb Chicken and Potatoes"],
-["Chicken", "Buffalo Chicken Casserole"], ["Chicken", "Monterey Chicken Skillet"]]
+["Chicken", "Buffalo Chicken Casserole"], ["Chicken", "Monterey Chicken Skillet"], ["Beef", "American Goulash"], ["Beef", "Chedder Cheeseburger Meatloaf"], ["Beef", "Burrito Casserole"], ["Beef", "Taco Pasta"], ["Pork", "Sausage with Tuscan Vegetables"], ["Pork", "Bacon Wrapped Sausage with Apples"], ["Pork", "Herb Roasted Porkloin"], ["Pork", "Brown Surgar Roasted Pork Loin"]]
