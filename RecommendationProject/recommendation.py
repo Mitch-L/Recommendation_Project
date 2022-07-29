d6784edc9e2a248e9cd71f03f89bebe2457cb345
Mitch_L <101993290+Mitch-L@users.noreply.github.com>
2022-07-28 21:28:59 -0500

    Creation of meal dictionary.

--- RecommendationProject/recommendation.py
+++ RecommendationProject/recommendation.py
@@ -1,1 +1,10 @@
-from data import recipes, types
+from data import meals
+
+def meal_dictionary(lst):
+    meal_dict = {}
+    for meal in lst:
+        if meal[0] not in meal_dict.keys():
+            meal_dict[meal[0]] = [meal[1:]]
+            continue
+        meal_dict[meal[0]] += [meal[1:]]
+    return meal_dict
