b8c27a31e775f3332363bf4e5b848148826d8c26
Mitch_L <101993290+Mitch-L@users.noreply.github.com>
2022-08-01 10:08:30 -0500

    Genre selection complete!!!

--- RecommendationProject/recommendation.py
+++ RecommendationProject/recommendation.py
@@ -1,5 +1,5 @@
 from data import games, genres
-
+### Functions ###
 def genre_dictionary(lst):
     genre_dict = {}
     for genre in lst:
@@ -18,13 +18,23 @@ def game_dictionary(lst):
         game_dict[game[0]] += [game[1:]]
     return game_dict
 
+def display(lst):
+    for i in lst:
+        print('\n')
+        print(f'Title: {i[0]}')
+        print(f'Review Score: {i[1]} / 2')
+        print(f'Dev: {i[-1]}')
+        print('\n')
+
+### Global Values ###
 game_shelf = game_dictionary(games)
 
+
 def main():
-    # Game selection
+ 
+    ### Genre Selection ###
     while True:
-        genre_types = input("What would you like to play? \nEnter first letter: ").strip().lower()
-        
+        genre_types = input(f"What would you like to play? \n \n{genre_dictionary(genres)} \n \n Enter first letter: ").strip().lower()
         try:
             genre_choices = genre_dictionary(genres)[genre_types]
         except KeyError as e:
@@ -32,6 +42,7 @@ def main():
         else:
             break
 
+    ### Subselection ###
     print(f"Your options are ", end="")
     for i in range(len(genre_choices)):
         if i < len(genre_choices) - 1:
@@ -39,15 +50,24 @@ def main():
             continue
         print("and " + genre_choices[i] + ".", end="\n")
     
+    ### Confirmation of Choice ###
     genre_choice = input("Which one \n Enter Name: " ).strip().lower()
     while True:
         if genre_choice in genre_choices:
-            confirmation = input(f"Do you want to see {genre_types} games? \n y/n: ".strip().lower())
+            confirmation = input(f"Do you want to see {genre_choice} games? \n y/n: ".strip().lower())
             break
         else:
             print("No such option available.")
         genre_choice = input("Which one do you want? \n Enter Name: " ).strip().lower()
-        
+    
+    ### Game Selection ###
+    while True:
+        if confirmation == 'y':
+            print(f"The selection of {genre_choice} games are: ")
+            display(game_shelf[genre_choice])
+            while True:
+                search = input(" Would to keep looking? \n Y/N" )
+
 
     
    
