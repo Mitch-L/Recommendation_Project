c272c8c32d35d620be29433b45398bf145423e58
Mitch_L <101993290+Mitch-L@users.noreply.github.com>
2022-08-01 10:11:14 -0500

    Cleaning up data.

--- RecommendationProject/data.py
+++ RecommendationProject/data.py
@@ -1,4 +1,17 @@
 from os import link
 genres = ['action', 'action-adventure', 'shooter firstperson', 'shooter thirdperson', 'rpg firstperson', 'rpg thirdperson', 'rpg turnbased', 'stategy', 'simulation']
 
-games = [['action', 'Elden Ring', '10', 'Bandai Namco'], ['action', 'Star Wars Jedi: Fallen Order', '9', 'EA'], ['action-adventure', 'Grand Theft Auto V', '9', 'Rockstar Games'], ['action-adventure', 'Red Dead Redemption Two', '9', 'Rockstar Games'], ['shooter firstperson', 'Doom Eternal', '9', 'Bethesda Softworks'], ['shooter firstperson', 'Call of Duty: Modern Warfare II', 'N/A', 'Activision'], ['shooter thirdperson', 'Aliens: Fireteam Elite', '6', 'Cold Iron Studios'], ['shooter person', 'The Division Two', '9', 'Ubisoft'], ['rpg firstperson', 'The Elder Scrolls V: Skyrim Special Edition', '8', 'Bethesda Softworks']['rpg firstperson', 'Cyberpunk 2077', '5', 'CD Project Red Studio'], ['rpg thirdperson', 'The Witcher Three: Wild Hunt', '10', 'CD Project Red Studio'], ['rpg thirdperson', 'Mass Effect Legendary Edition', '9', 'EA'], ['rpg turnbased', 'Yakuza: Like A Dragon', '9', 'Sega'], ['rpg turnbased', 'Dragon Quest XI', '9', 'Square Enix'], ['stategy', 'Total War: Three Kingdoms' '8', 'Sega'], ['stategy', 'Age of Empires IV', '8', 'Xbox Game Studios'], ['simulation', 'Forza Horizon Five', '9', 'Xbox Game Studios'], ['simulation', 'Microsoft Flight Simulator', '7', 'Xbox Game Studios']]
+games = [['action', 'Elden Ring', '10', 'Bandai Namco'], 
+['action', 'Star Wars Jedi: Fallen Order', '9', 'EA'], 
+['action-adventure', 'Grand Theft Auto V', '9', 'Rockstar Games'], ['action-adventure', 'Red Dead Redemption 2', '9', 'Rockstar Games'], 
+['shooter firstperson', 'Doom Eternal', '9', 'Bethesda Softworks'], 
+['shooter firstperson', 'Call of Duty: Modern Warfare II', 'N/A', 'Activision'], ['shooter thirdperson', 'Aliens: Fireteam Elite', '6', 'Cold Iron Studios'], ['shooter thirdperson', 'The Division Two', '9', 'Ubisoft'], 
+['rpg firstperson', 'The Elder Scrolls V: Skyrim', '8', 'Bethesda Softworks'], 
+['rpg firstperson', 'Cyberpunk 2077', '5', 'CD Project Red Studio'], 
+['rpg thirdperson', 'The Witcher 3: Wild Hunt', '10', 'CD Project Red Studio'], ['rpg thirdperson', 'Mass Effect Legendary Edition', '9', 'EA'], 
+['rpg turnbased', 'Yakuza: Like A Dragon', '9', 'Sega'], 
+['rpg turnbased', 'Dragon Quest XI', '9', 'Square Enix'], 
+['stategy', 'Total War: Three Kingdoms' '8', 'Sega'], 
+['stategy', 'Age of Empires IV', '8', 'Xbox Game Studios'], 
+['simulation', 'Forza Horizon Five', '9', 'Xbox Game Studios'], 
+['simulation', 'Microsoft Flight Simulator', '7', 'Xbox Game Studios']]
